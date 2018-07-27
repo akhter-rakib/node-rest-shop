0171d6dd546fa89623070eaf5b2d24d7c8a4aa39
Aktaruzzaman Rakib <rakibccj@gmail.com>
2018-07-27 17:30:26 +0600

    createServer

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const app = require('./app.js');
+
+const port = process.env.PORT || 1234;
+
+const server = http.createServer(app);
+
+server.listen(port);
+
+console.log(port);
+
+
