a80cae5f669dd6c392f667112a49c6cac46d31f8
Aktaruzzaman Rakib <rakibccj@gmail.com>
2018-07-28 16:18:19 +0600

    parse body to income request

--- app.js
+++ app.js
@@ -3,8 +3,13 @@
  
  const app = express();
  
+ const bodyParser = require('body-parser');
+ 
  const productRoutes = require('./api/routes/products');
  const ordersRoutes = require('./api/routes/orders');
+ //use to parse body into incoming request
+ app.use(bodyParser.urlencoded({extended: false}));
+ app.use(bodyParser.json());
  
  app.use('/products', productRoutes);
  app.use('/orders', ordersRoutes);
